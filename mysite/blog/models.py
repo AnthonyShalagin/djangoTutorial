87d00b1652e2ab7b6c650675ebeaaf86c196a5c5
Anthony Shalagin <anthony.shalagin@gmail.com>
2016-11-05 18:51:23 -0400

    Added model for the blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 140)
+    body = models.TextField()
+    date = models.DateTimeField()
+
+
+#If we reference post.title, it'll be an object
+#We use the method below to convert it to a string 
+    def __str__(self):
+        return self.title
